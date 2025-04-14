406c32973838738074ad9ce9e8446373ba323273
ittou-shura <mukherjeerishiraj8@gmail.com>
2025-04-14 20:32:00 +0530

    latest

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chattyweb-inky.vercel.app/api",
+  baseURL: "https://chatwebb.onrender.com/api",
   withCredentials: true,
 });
--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "https://chattyweb-inky.vercel.app";
+const BASE_URL = "https://chatwebb.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
